159088ca2206f60e30fc76898c49059a4b737b18
Dhanuja Isuru <128521002+Dhanujaisuru@users.noreply.github.com>
2025-11-06 14:09:05 +0530

    fix the issue

--- new.py
+++ new.py
@@ -25,5 +25,7 @@ if __name__ == "__main__":
     a = float(input("Enter first number: "))
     b = float(input("Enter second number: "))
 
-print("hello my dear friend")
-print("This is a new file added for GitHub practice.")
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {subtract(a, b)}")
+    print(f"{a} * {b} = {multiply(a, b)}")
+    print(f"{a} / {b} = {divide(a, b)}")
